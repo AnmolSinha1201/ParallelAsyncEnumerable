247c323f17095829ba8ab24ad64a51c26572752f
Anmol Sinha <anmolsinha1201@outlook.com>
2023-05-19 11:12:56 +0300

    Split operators into different files

---
+++ Source/Select.cs
@@ -0,0 +1,20 @@
+public static partial class Helpers
+{
+	public static async IAsyncEnumerable<TOut> SelectParallelAsync<TIn, TOut>(
+	this IAsyncEnumerable<TIn> enumerable, Func<TIn, ValueTask<TOut>> predicate)
+	{
+		var sem = new SemaphoreSlim(10, 10);
+		var retVal = await enumerable.Select(item => {
+			return Task.Run(async () => {
+				await sem.WaitAsync();
+				var retVal = await predicate(item);
+				sem.Release();
+
+				return retVal;
+			});
+		}).ToListAsync();
+
+		foreach (var item in retVal)
+			yield return await item;	
+	}
+}
